6262d30e0e8b806e12c32b7721a14d107d88ae59
Ryu Martinez <ryueduardomartinez@outlook.es>
2024-02-05 14:20:07 -0300

    Update Reviews.svelte

--- src/components/Reviews.svelte
+++ src/components/Reviews.svelte
@@ -73,19 +73,16 @@
 </script>
 
 <SectionWrapper id="reviews">
-    <div
-        class="flex flex-col gap-10 sm:gap-14 md:gap-24 py-20 flex-1 items-center justify-center"
-    >
+    <div class="flex flex-col gap-10 sm:gap-14 md:gap-24 py-20 flex-1 items-center justify-center">
         <div class="flex flex-col gap-2">
             <p class="opacity-60 text-base sm:text-lg md:text-xl text-center">
                 Don't just take our word for it
             </p>
-            <h3
-                class="text-4xl sm:text-5xl md:text-6xl max-w-[1000px] mx-auto w-full font-semibold text-center"
-            >
+            <h3 class="text-4xl sm:text-5xl md:text-6xl max-w-[1000px] mx-auto w-full font-semibold text-center">
                 See What <span class="text-indigo-400">Others</span> Have To Say
             </h3>
         </div>
+        <!-- Loop para crear los cards -->
         <div class="flex flex-col md:flex-row gap-8 w-fit mx-auto">
             <div class="flex flex-col gap-8">
                 {#each lim ? reviewsList.slice(0, 4) : reviewsList as reviewItem, index}
