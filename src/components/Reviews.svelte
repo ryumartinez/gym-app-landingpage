0dbd202e27838fc3ebe8489a7cddc6de7405ff82
Ryu Martinez <ryueduardomartinez@outlook.es>
2024-02-06 08:43:41 -0300

    Update Reviews.svelte

--- src/components/Reviews.svelte
+++ src/components/Reviews.svelte
@@ -76,10 +76,10 @@
     <div class="flex flex-col gap-10 sm:gap-14 md:gap-24 py-20 flex-1 items-center justify-center">
         <div class="flex flex-col gap-2">
             <p class="opacity-60 text-base sm:text-lg md:text-xl text-center">
-                Don't just take our word for it
+                Rambosa
             </p>
             <h3 class="text-4xl sm:text-5xl md:text-6xl max-w-[1000px] mx-auto w-full font-semibold text-center">
-                See What <span class="text-indigo-400">Others</span> Have To Say
+                Nuestro <span class="text-indigo-400">Menú</span> del día
             </h3>
         </div>
         <!-- Loop para crear los cards -->
