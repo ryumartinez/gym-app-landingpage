6490212ef8c81a108b547867ad78eb4f837f22d6
ryumartinez <ryueduardomartinez@outlook.es>
2024-02-07 07:42:55 -0300

    learned how props passing works

--- src/components/Reviews.svelte
+++ src/components/Reviews.svelte
@@ -5,74 +5,10 @@
 
     let lim = true;
 
-    let reviewsList = [
-        {
-            name: "Pollo al horno",
-            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in",
-            features: [
-                "Personalized workout plans",
-                "Community features",
-                "Workout recommendation system",
-            ],
-        },
-        {
-            name: "Omelette",
-            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in",
-            features: [
-                "Guided workouts",
-                "Meal logging and tracking",
-                "User-friendly interface",
-            ],
-        },
-        {
-            name: "Tallarín",
-            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in",
-            features: [
-                "Variety of workout routines and challenges",
-                "Nutrition tips",
-                "Progress tracking and challenges",
-            ],
-        },
-        {
-            name: "Estofado",
-            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in",
-            features: [
-                "Quick and effective workouts",
-                "Timer feature",
-                "Variety of workouts",
-            ],
-        },
-        {
-            name: "Milanesa",
-            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in",
-            features: [
-                "Advanced workout programs",
-                "Customization options",
-                "Detailed data analysis",
-            ],
-        },
-        {
-            name: "Guiso de arroz",
-            review: "Swoley Moley has helped me overcome my fear of going to the gym. The encouraging community and supportive environment have made me feel comfortable and confident. I love the social features that allow me to connect with other fitness enthusiasts and share my progress.",
-            features: [
-                "Supportive community",
-                "Social features",
-                "Encouraging environment",
-            ],
-        },
-        {
-            name: "Asado a la olla",
-            review: "Swoley Moley is more than just a fitness app; it's a lifestyle change. It's helped me develop healthy habits and make better choices about my diet and exercise. I'm incredibly grateful for this app and the positive impact it's had on my life.",
-            features: [
-                "Focus on healthy habits",
-                "Nutritional guidance",
-                "Positive lifestyle impact",
-            ],
-        },
-    ];
+    export let reviewsList
 </script>
 
-<SectionWrapper id="reviews">
+<SectionWrapper id="reviews" {reviewsList}>
     <div class="flex flex-col gap-10 sm:gap-14 md:gap-24 py-20 flex-1 items-center justify-center">
         <div class="flex flex-col gap-2">
             <p class="opacity-60 text-base sm:text-lg md:text-xl text-center">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,72 @@
     import Product from "../components/Product.svelte";
     import Reviews from "../components/Reviews.svelte";
     export let data
+
+    let reviewsList = [
+        {
+            name: "Pollo al horno",
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in",
+            features: [
+                "Personalized workout plans",
+                "Community features",
+                "Workout recommendation system",
+            ],
+        },
+        {
+            name: "Omelette",
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in",
+            features: [
+                "Guided workouts",
+                "Meal logging and tracking",
+                "User-friendly interface",
+            ],
+        },
+        {
+            name: "Tallarín",
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in",
+            features: [
+                "Variety of workout routines and challenges",
+                "Nutrition tips",
+                "Progress tracking and challenges",
+            ],
+        },
+        {
+            name: "Estofado",
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in",
+            features: [
+                "Quick and effective workouts",
+                "Timer feature",
+                "Variety of workouts",
+            ],
+        },
+        {
+            name: "Milanesa",
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in",
+            features: [
+                "Advanced workout programs",
+                "Customization options",
+                "Detailed data analysis",
+            ],
+        },
+        {
+            name: "Guiso de arroz",
+            review: "Swoley Moley has helped me overcome my fear of going to the gym. The encouraging community and supportive environment have made me feel comfortable and confident. I love the social features that allow me to connect with other fitness enthusiasts and share my progress.",
+            features: [
+                "Supportive community",
+                "Social features",
+                "Encouraging environment",
+            ],
+        },
+        {
+            name: "Asado a la olla",
+            review: "Swoley Moley is more than just a fitness app; it's a lifestyle change. It's helped me develop healthy habits and make better choices about my diet and exercise. I'm incredibly grateful for this app and the positive impact it's had on my life.",
+            features: [
+                "Focus on healthy habits",
+                "Nutritional guidance",
+                "Positive lifestyle impact",
+            ],
+        },
+    ];
 </script>
 
 <main class="flex flex-col">
@@ -13,7 +79,7 @@
 	{/each}
     <!-- <Hero />
     <Product /> -->
-    <Reviews />
+    <Reviews  {reviewsList}/>
     <!-- <FaQs /> -->
     <!-- <Conversion /> -->
 </main>
