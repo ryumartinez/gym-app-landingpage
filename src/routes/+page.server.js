535ed9c833df35269980d90c09e080ecf4268b2f
ryumartinez <ryueduardomartinez@outlook.es>
2024-02-07 07:20:35 -0300

    Update +page.server.js

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,1 +1,10 @@
-//aca es en donde tiene que ir el data fetching
+export async function load({ setHeaders }) {
+	setHeaders({
+        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*'
+	});
+    const response = await fetch('http://localhost:5136/api/menu-item');
+    const menu = await response.json();
+    return {menu}
+}
